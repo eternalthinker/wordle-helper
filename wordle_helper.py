88bf06c200f1a682eff79ce6a08c5a86b1469445
Rahul Anand <rahul@canva.com>
2022-01-19 10:56:25 +1100

    Consider letter popularity while sorting suggested words

--- wordle_helper.py
+++ wordle_helper.py
@@ -8,23 +8,60 @@ def get_5_letter_words():
         words_5letters_file.write(word + '\n')
 
 def get_5_letter_words_freq_csv():
-  with open("unigram_freq.csv", 'r') as all_words_file, open("words_5letters_freq.txt", 'w') as words_5letters_file:
-    all_lines = all_words_file.read().split('\n')
-    for line in all_lines:
+  with open("corncob_lowercase.txt", 'r') as all_words_file,\
+      open("unigram_freq.csv", 'r') as freq_words_file,\
+      open("words_5letters_freq.txt", 'w') as words_5letters_file:
+    all_words = all_words_file.read().split('\n')
+    all_5letter_words = [word for word in all_words if len(word) == 5]
+    
+    word_freq_map = {}
+    all_freq_lines = freq_words_file.read().split('\n')
+    for line in all_freq_lines:
       if line.strip() == '':
         continue
       word, freq = line.split(',')
       if len(word) == 5:
-        words_5letters_file.write(word + '\n')
+        word_freq_map[word] = int(freq)
+    
+    def get_word_freq(w):
+      if w in word_freq_map:
+        return word_freq_map[w]
+      return 0
+    
+    all_5letter_words.sort(reverse=True, key=get_word_freq)
+    for word in all_5letter_words:
+      words_5letters_file.write(word + '\n')
+    
 
+common_letters_map = {
+  'e': 11.16,
+  'a': 8.45,
+  'r': 7.58,
+  'i': 7.54,
+  'o': 7.16,
+  't': 6.95,
+  'n': 6.65,
+  's': 5.74,
+  'l': 5.49,
+  'c': 4.54,
+  'u': 3.63,
+  'd': 3.38,
+}
 
-def print_suggested_words(words_5letters, exclude_letters, exclude_positions, include_positions, limit=None):
+def get_common_letter_score(word):
+  score = 0
+  for letter in word:
+    if letter in common_letters_map:
+      score += common_letters_map[letter]
+  return score
+
+def get_suggested_words(words_5letters, exclude_letters, exclude_positions, include_positions, limit=25):
   include_letters = set()
   for wrong_pos_letter_set in exclude_positions:
     include_letters |= wrong_pos_letter_set
   num_include_letters = len(include_letters)
   
-  count = 0
+  suggested_words = []
   for word in words_5letters:
     word = word.lower()
     skip_word = False
@@ -49,11 +86,15 @@ def print_suggested_words(words_5letters, exclude_letters, exclude_positions, in
         break
     if skip_word:
       continue
+    suggested_words.append(word)
+  
+  for letter in exclude_letters:
+    if letter in common_letters_map:
+      del common_letters_map[letter]
 
-    print(word)
-    count += 1
-    if limit is not None and count == limit:
-      break
+  selected_suggested_words = suggested_words[:25]
+  selected_suggested_words.sort(reverse=True, key=get_common_letter_score)
+  return suggested_words, selected_suggested_words
 
 
 def suggest_words():
@@ -77,8 +118,6 @@ def suggest_words():
     words_5letters = words_5letters_file.read().split('\n')
     print_suggested_words(words_5letters, exclude_letters, exclude_positions, include_positions)
 
-
-
 def suggest_words_interactive():
   exclude_letters = set([])
   exclude_positions = [
@@ -100,6 +139,7 @@ def suggest_words_interactive():
   with open('words_5letters_freq.txt', 'r') as words_5letters_file:
     words_5letters = words_5letters_file.read().split('\n')
 
+  suggested_words = None
   while True:
     exclude_letters_new_ip = input("\nWhich new letters must be EXCLUDED in the word? (separate with space): ")
     exclude_letters_new = exclude_letters_new_ip.split()
@@ -116,7 +156,15 @@ def suggest_words_interactive():
       include_positions[pos] = None if include_pos_new == '' else include_pos_new
 
     print("\nSuggested words are:")
-    print_suggested_words(words_5letters, exclude_letters, exclude_positions, include_positions, limit=25)
+    words_list = words_5letters if (suggested_words is None) else suggested_words
+    suggested_words, selected_suggested_words = get_suggested_words(
+        words_list,
+        exclude_letters,
+        exclude_positions,
+        include_positions,
+    )
+    for word in selected_suggested_words:
+      print(word)
 
     should_continue = input("\nContinue? (Enter 'n' to exit): ")
     if should_continue.strip().lower() == 'n':
